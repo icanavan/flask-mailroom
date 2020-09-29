aec7394aaa79c7c4aa2debe9654c43f8b491f2c3
Ian Canavan <icanavan.327@gmail.com>
2020-09-28 21:57:10 -0700

    Assignment not quite done and published - will try to finish before class. Links have been added no problem, main issue now is once the donor is submitted it's getting a Bad Form Request error so have to fix that.

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donation
+from model import Donor
 
 app = Flask(__name__)
 
@@ -15,7 +16,19 @@ def home():
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+
+    if request.method == 'POST':
+        donor = Donor.select().where(Donor.name == request.form['name']).get()
+        donor.save()
+        donation = Donation(value=request.form['donation-amount'], donor=request.form['name'])
+        donation.save()
+        return redirect(url_for('donations'))
+    else:
+        return render_template('create.jinja2')
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
